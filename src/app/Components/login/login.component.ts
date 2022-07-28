5e2e3e57a2b71891da6c25290ea7b11d7fe9a7de
Rupali Natewad <rupalirnatewad@gmail.com>
2022-07-28 15:39:19 +0530

    updated

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router'; 
 
 @Component({
   selector: 'app-login',
@@ -7,40 +8,34 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  registerForm: FormGroup;
+ 
+  loginForm!: FormGroup;
   submitted = false;
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.registerForm = this.formBuilder.group({
+    this.loginForm = this.formBuilder.group({
      
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required], 
-      password: ['', [Validators.required, Validators.minLength(6)]],
-      confirmPassword: ['', Validators.required],
-      acceptTerms: [false, Validators.requiredTrue]
+      email: ['', [Validators.required, Validators.email]],
+     
     })
 }
 
   // convenience getter for easy access to form fields
-  get f() { return this.registerForm.controls; }
+  
 
   onSubmit() {
     this.submitted = true;
 
     // stop here if form is invalid
-    if (this.registerForm.invalid) {
-      return;
-    }
+    
+     
 
 
 
-  }
-
-  onReset() {
-    this.submitted = false;
-    this.registerForm.reset();
   }
 
 }
+
+
